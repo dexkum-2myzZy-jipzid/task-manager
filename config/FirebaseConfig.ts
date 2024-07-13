0bdbfa7d2d19f1e76a35ce60939d5616bed019d7
Mario <liang.chen829@gmail.com>
2024-07-12 21:12:54 -0700

    chore: Add react-native-dotenv for environment variables and store keys in env

--- config/FirebaseConfig.ts
+++ config/FirebaseConfig.ts
@@ -3,18 +3,28 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import {
+  API_KEY,
+  AUTH_DOMAIN,
+  PROJECT_ID,
+  STORAGE_BUCKET,
+  MESSAGING_SENDER_ID,
+  APP_ID,
+  MEASUREMENT_ID,
+} from "@env";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyAFg1W_R_tKroGnfX4wMN42DqjbJsHbGig",
-  authDomain: "task-manager-5494b.firebaseapp.com",
-  projectId: "task-manager-5494b",
-  storageBucket: "task-manager-5494b.appspot.com",
-  messagingSenderId: "70930723227",
-  appId: "1:70930723227:web:346f1ca11453cdaa10f13e",
-  measurementId: "G-713ZK7JS43",
+
+export const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
 };
 
 // Initialize Firebase
